62a43e21424acdec031c2b59c4fc8608494f7fcd
clem-alleynejr <clemjr@hotmail.com>
2023-07-11 19:27:45 -0600

    troubleshooting login/signup

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,22 +2,28 @@ import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service'
 
 export default function NavBar({ user, setUser }) {
-    function handleLogOut() {
-        // Delegate to the users-service
-        userService.logOut();
-        // Update state will also cause a re-render
-        setUser(null);
-      }
-  
-    return (
+  function handleLogOut() {
+    // Delegate to the users-service
+    userService.logOut();
+    // Update state will also cause a re-render
+    setUser(null);
+  }
+
+  return (
     <nav>
-      <Link to="/orders">Order History</Link>
+      <Link to="/">Home</Link>
       &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp; | &nbsp;
-      <p>{user.name}</p>
-      &nbsp; | &nbsp;
-      <Link to="" onClick={handleLogOut}>Log Out</Link>
+      {user ?
+        <>
+          <Link to="/workouts">My Workouts</Link>
+          &nbsp; | &nbsp;
+          <p>{user.name}</p>
+          &nbsp; | &nbsp;
+          <Link to="" onClick={handleLogOut}>Log Out</Link>
+        </>
+        :
+        <Link to="/auth">Log In/Sign Up</Link>
+      }
     </nav>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,8 @@
 export default function HomePage() {
     return (
         <>
-        <h1>Welcome to Fitness Witness!</h1>
-        <p>Keep yourself accountable on your fitness journey with the help of this app</p>
+        <h1>Welcome to Fitness Witness</h1>
+        <p>Keep yourself accountable on your fitness journey with the help of this app!</p>
         </>
     )
 } 
