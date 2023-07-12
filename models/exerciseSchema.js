1469c57321da6e836293d82c04dae2573e8caee0
clem-alleynejr <clemjr@hotmail.com>
2023-07-12 17:30:47 -0600

    troublshooting seed pt 3

--- models/exerciseSchema.js
+++ models/exerciseSchema.js
@@ -4,7 +4,6 @@ const exerciseSchema =  new Schema({
     bodyPart: { type: String, required: true },
     equipment: { type: String, required: true },
     gifUrl: { type: String, required: true },
-    id: { type: String, required: true },
     name: { type: String, required: true },
     target: { type: String, required: true }
 }, {
