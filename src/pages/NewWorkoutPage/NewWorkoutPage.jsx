39f384281709f3b4f7450400c42701ff0a58ec85
clem-alleynejr <clemjr@hotmail.com>
2023-07-13 09:03:44 -0600

    exercise target bodyparts now remembered using a ref

--- src/pages/NewWorkoutPage/NewWorkoutPage.jsx
+++ src/pages/NewWorkoutPage/NewWorkoutPage.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as exercisesAPI from '../../utilities/exercises-api'
 
 export default function NewWorkoutPage() {
     const [exerciseOptions, setExerciseOptions] = useState([]);
+    const bodyPartRef = useRef([]);
 
     useEffect(function() {
         async function getExercises() {
             const exercises = await exercisesAPI.getAll();
+            bodyPartRef.current = [...new Set(exercises.map(exercise => exercise.bodyPart))]
             setExerciseOptions(exercises);
         }
         getExercises();
