8863b1a7d4b8d27346485f94dd432e533cd24840
clem-alleynejr <clemjr@hotmail.com>
2023-07-14 15:33:00 -0600

    basic ExerciseChoice component completed. still needs functionality

--- src/components/ExerciseChoice/ExerciseChoice.jsx
+++ src/components/ExerciseChoice/ExerciseChoice.jsx
@@ -1,28 +1,30 @@
 import './ExerciseChoice.css';
 
 export default function ExerciseChoice({ exerciseChoice, isSaved }) {
-  return (
-    <div className="ExerciseChoice">
-      <div className="flex-ctr-ctr">{exerciseChoice.exercise.bodyPart}</div>
-      <div className="flex-ctr-ctr">{exerciseChoice.exercise.name}</div>
-      <div className="sets" style={{ justifyContent: isSaved && 'center' }}>
-        {!isSaved &&
-          <input type="text" name="sets"/>
-        }
-      <div className="reps" style={{ justifyContent: isSaved && 'center' }}>
-        {!isSaved &&
-          <input type="text" name="reps"/>
-        }        
-        <span>{lineItem.qty}</span>
-        {!isPaid &&
-          <button
-            className="btn-xs"
-            onClick={() => alert('clicked')}
-          >+</button>
-        }
-      </div>
-      </div>
-      <div className="ext-price">${lineItem.extPrice.toFixed(2)}</div>
-    </div>
-  );
+    return (
+        <div className="ExerciseChoice">
+            <div className="flex-ctr-ctr">{exerciseChoice.exercise.name}</div>
+            <div className="flex-ctr-ctr">{exerciseChoice.exercise.bodyPart}</div>
+            <div className="flex-ctr-ctr">{exerciseChoice.exercise.equipment}</div>
+
+            <div className="sets">
+                <p>Sets</p>
+                {isSaved ? 
+                    <span>{exerciseChoice.setQty}</span>
+                    :
+                    <input type="text" name="sets" />
+                }
+            </div>      
+
+            <div className="reps">
+                <p>Reps</p>
+                {isSaved ?
+                    <span>{exerciseChoice.repQty}</span>
+                    :
+                    <input type="text" name="reps" />
+                }
+            </div>
+
+        </div>
+    );
 }
