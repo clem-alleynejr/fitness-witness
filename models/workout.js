35fa0d0882a4e10782050c04d6ad040eabc441c5
clem-alleynejr <clemjr@hotmail.com>
2023-07-14 17:14:48 -0600

    workout model complete

--- models/workout.js
+++ models/workout.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const exerciseSchema = require('./exerciseSchema')
+
+const exerciseChoiceSchema = new Schema({
+    // sets and reps will be strings to allow users to input range (ex. '8-10')
+    setQty: { type: String, default: '' },
+    repQty: { type: String, default: '' },
+    exercise: exerciseSchema
+}, {
+    timestamps: true
+})
 
 const workoutSchema =  new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    exercises: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Exercise',
-        required: true
-    }],
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    exerciseChoice: [exerciseChoiceSchema],
+    isSaved: { type: Boolean, default: false },
 }, {
     timestamps: true
 })
