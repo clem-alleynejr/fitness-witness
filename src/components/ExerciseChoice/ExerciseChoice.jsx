3a8d70dcbf2806291636253eaea4734d59c689f2
clem-alleynejr <clemjr@hotmail.com>
2023-07-18 16:44:43 -0600

    trying out redeployment

--- src/components/ExerciseChoice/ExerciseChoice.jsx
+++ src/components/ExerciseChoice/ExerciseChoice.jsx
@@ -6,22 +6,30 @@ export default function ExerciseChoice({ exerciseChoice, isSaved, handleChangeSe
         handleChangeSetQty(exerciseChoice.exercise._id, evt.target.value)
     }
 
+    function capitalizeFirstLetter(string) {
+        return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    }
+
+    const capitalizedExerciseName = capitalizeFirstLetter(exerciseChoice.exercise.name);
+    const capitalizedBodyPart = capitalizeFirstLetter(exerciseChoice.exercise.bodyPart);
+    const capitalizedTarget = capitalizeFirstLetter(exerciseChoice.exercise.target);
+    const capitalizedEquipment = capitalizeFirstLetter(exerciseChoice.exercise.equipment);
 
     return (
         <div className='card'>
             <div className="card-body">
 
-                <h5 className="flex-ctr-ctr">{exerciseChoice.exercise.name}</h5>
-                <p className="flex-ctr-ctr">Body Part: {exerciseChoice.exercise.bodyPart}</p>
-                <p className="target">Target: {exerciseChoice.exercise.target}</p>
-                <p className="flex-ctr-ctr">Equipment: {exerciseChoice.exercise.equipment}</p>
+                <h5 className="flex-ctr-ctr">{capitalizedExerciseName}</h5>
+                <p className="flex-ctr-ctr">Body Part: {capitalizedBodyPart}</p>
+                <p className="target">Target: {capitalizedTarget}</p>
+                <p className="flex-ctr-ctr">Equipment: {capitalizedEquipment}</p>
 
                 <div className="sets">
                     {isSaved ?
                         <p>{exerciseChoice.setQty}</p>
                         :
-                        <div class="mb-3">
-                            <label for="sets" className='form-label'>Enter Sets:</label>
+                        <div className="mb-3">
+                            <label htmlFor="sets" className='form-label'>Enter Sets:</label>
                             <input id="sets" type="text" placeholder='Ex. "3"' className='form-control' defaultValue={editWorkout && workout ? exerciseChoice.setQty : ''} onChange={handleSetQty} />
                         </div>
                     }
@@ -31,8 +39,8 @@ export default function ExerciseChoice({ exerciseChoice, isSaved, handleChangeSe
                     {isSaved ?
                         <p>{exerciseChoice.repQty}</p>
                         :
-                        <div class="mb-3">
-                            <label for='reps' className='form-label'>Enter Reps:</label>
+                        <div className="mb-3">
+                            <label htmlFor='reps' className='form-label'>Enter Reps:</label>
                             <input id='sets' type="text" placeholder='Ex. "6-8"' className='form-control' defaultValue={editWorkout && workout ? exerciseChoice.repQty : ''} onChange={(evt) => handleChangeRepQty(exerciseChoice.exercise._id, evt.target.value)} />
                         </div>
                     }
