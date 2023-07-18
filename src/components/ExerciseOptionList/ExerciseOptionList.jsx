e6a580a6331b5afcfc3ec6e500d9f3cc11737d95
clem-alleynejr <clemjr@hotmail.com>
2023-07-18 17:18:44 -0600

    reretrying redployment

--- src/components/ExerciseOptionList/ExerciseOptionList.jsx
+++ src/components/ExerciseOptionList/ExerciseOptionList.jsx
@@ -1,6 +1,7 @@
 import './ExerciseOptionList.css';
 import ExerciseOption from '../ExerciseOption/ExerciseOption';
 
+
 export default function ExerciseOptionList({ exerciseOptions, handleAddToWorkout, useState, activeBodyPart }) {
 
   const [search, setSearch] = useState('')
