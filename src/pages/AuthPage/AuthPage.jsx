2c7aab9de83cac435060494c136683725ab15c7b
clem-alleynejr <clem.alleynejr@gmail.com>
2024-05-05 16:44:08 -0600

    re added AuthPage and HomePage files (accidentally deleted)

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import './AuthPage.css'
+
+export default function AuthPage({ setUser }) {
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  return (
+    <main>
+
+      <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Existing User? Log In' : 'No Account? Sign Up'}</button>
+      {showSignUp ? 
+        <SignUpForm setUser={setUser} />
+        :
+        <LoginForm setUser={setUser} />
+      }
+    </main>
+  );
+}
