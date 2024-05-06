746b70b00fe0e34d14119cde3762cac1b790e596
clem-alleynejr <clem.alleynejr@gmail.com>
2024-05-05 22:16:54 -0600

    finishedbasic nav-bar and moved its css over to index.css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service'
+import { Link } from "react-router-dom";
+import * as userService from "../../utilities/users-service";
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -10,39 +10,53 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <>
-      {user ?
-        <>
+          <nav className="main-nav">
+            <div className="navbar">
+              <Link to="/" className="navbar-brand">
+                <img
+                  src="https://images-platform.99static.com/Lw8CyZQrGrnlyPa-QHw8p4Tn7Nw=/200x200:1800x1800/500x500/top/smart/99designs-contests-attachments/121/121893/attachment_121893514"
+                  alt="Logo"
+                  width="30"
+                  height="24"
+                />
+                Fitness Witness
+              </Link>
 
+              <button className="nav-toggle-button"></button>
 
-        
-          <nav className="navbar bg-dark navbar-dark">
-            <div className="container-fluid">
-              <Link to="/" className="navbar-brand home">
-              <img src="https://images-platform.99static.com/Lw8CyZQrGrnlyPa-QHw8p4Tn7Nw=/200x200:1800x1800/500x500/top/smart/99designs-contests-attachments/121/121893/attachment_121893514" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
-                <span>Fitness Witness</span>
-                </Link>
-              <div className='ms-auto'>
-                <Link to="/workouts" className="navbar-brand">My Workouts</Link>
-                <span className="navbar-brand mb-0 h1">{user.name}</span>
-                <Link to="" className="navbar-brand" onClick={handleLogOut}>Log Out</Link>
+              <div className="collapsible-nav">
+                <ul className="nav-links">
+                <li>
+                  <Link
+                    to={user ? "/workouts" : "/auth"}
+                    className="navbar-brand"
+                  >
+                    My Workouts
+                  </Link>
+                </li>
+                </ul>
+
+                <ul className="auth-links">
+                  {user ? (
+                    <li>
+                      <Link
+                        to=""
+                        className="navbar-brand"
+                        onClick={handleLogOut}
+                      >
+                        Log Out
+                      </Link>
+                    </li>
+                  ) : (
+                    <li>
+                      <Link to="/auth" className="navbar-brand">
+                        Log In/Sign Up
+                      </Link>
+                    </li>
+                  )}
+                </ul>
               </div>
             </div>
           </nav>
-        </>
-        :
-        <>
-          <nav className="navbar bg-dark navbar-dark">
-            <div className="container-fluid">
-              <Link to="/" className="navbar-brand">Fitness Witness</Link>
-              <div className='ms-auto'>
-                <Link to="/auth" className="navbar-brand">Log In/Sign Up</Link>
-              </div>
-            </div>
-          </nav>
-        </>
-      }
-    </>
   );
 }
-
