ab88bb0a2b8dd761ea346b596a1dd6255f5c01ce
clem-alleynejr <clem.alleynejr@gmail.com>
2024-05-08 15:51:23 -0600

    commented out a lot of previous code from sever.js, models, routes, and controllers

--- controllers/api/workouts.js
+++ controllers/api/workouts.js
@@ -2,15 +2,15 @@ const Workout = require('../../models/workout');
 // const Item = require('../../models/item');
 
 module.exports = {
-    unsavedWorkout,
-    addToUnsavedWorkout,
-    saveUnsavedWorkout,
-    setWorkoutName,
-    setSetQtyInUnsavedWorkout,
-    setRepQtyInUnsavedWorkout,
+    // unsavedWorkout,
+    // addToUnsavedWorkout,
+    // saveUnsavedWorkout,
+    // setWorkoutName,
+    // setSetQtyInUnsavedWorkout,
+    // setRepQtyInUnsavedWorkout,
+    // deleteSavedWorkout,
+    // editWorkout,
     index,
-    deleteSavedWorkout,
-    editWorkout,
     new: newWorkout,
     show,
     edit,
@@ -51,59 +51,59 @@ function deleteWorkout(req, res) {
 
 
 
-async function unsavedWorkout(req, res) {
-    const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
-    res.json(unsavedWorkout);
-}
+// async function unsavedWorkout(req, res) {
+//     const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
+//     res.json(unsavedWorkout);
+// }
 
-// Add an exercise to the unsaved workout
-async function addToUnsavedWorkout(req, res) {
-    const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
-    await unsavedWorkout.addExerciseToUnsavedWorkout(req.params.id);
-    res.json(unsavedWorkout);
-}
+// // Add an exercise to the unsaved workout
+// async function addToUnsavedWorkout(req, res) {
+//     const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
+//     await unsavedWorkout.addExerciseToUnsavedWorkout(req.params.id);
+//     res.json(unsavedWorkout);
+// }
 
-// Updates the unsaved Workout's isSaved property to true (turning it into a saved workout)
-async function saveUnsavedWorkout(req, res) {
-    const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
-    unsavedWorkout.isSaved = true;
-    await unsavedWorkout.save();
-    res.json(unsavedWorkout)
-}
+// // Updates the unsaved Workout's isSaved property to true (turning it into a saved workout)
+// async function saveUnsavedWorkout(req, res) {
+//     const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
+//     unsavedWorkout.isSaved = true;
+//     await unsavedWorkout.save();
+//     res.json(unsavedWorkout)
+// }
 
-// Update the workout name in the unsaved workout
-async function setWorkoutName(req, res) {
-    const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
-    await unsavedWorkout.setWorkoutName(req.body.workoutId, req.body.newWorkoutName);
-    res.json(unsavedWorkout)
-}
+// // Update the workout name in the unsaved workout
+// async function setWorkoutName(req, res) {
+//     const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
+//     await unsavedWorkout.setWorkoutName(req.body.workoutId, req.body.newWorkoutName);
+//     res.json(unsavedWorkout)
+// }
 
-// Update the exercise's number of sets in the unsaved workout
-async function setSetQtyInUnsavedWorkout(req, res) {
-    const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
-    await unsavedWorkout.setSetQty(req.body.exerciseId, req.body.newSetQty);
-    res.json(unsavedWorkout)
-}
+// // Update the exercise's number of sets in the unsaved workout
+// async function setSetQtyInUnsavedWorkout(req, res) {
+//     const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
+//     await unsavedWorkout.setSetQty(req.body.exerciseId, req.body.newSetQty);
+//     res.json(unsavedWorkout)
+// }
 
-// Update the exercise's number of reps in the unsaved workout
-async function setRepQtyInUnsavedWorkout(req, res) {
-    const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
-    await unsavedWorkout.setRepQty(req.body.exerciseId, req.body.newRepQty);
-    res.json(unsavedWorkout)
-}
+// // Update the exercise's number of reps in the unsaved workout
+// async function setRepQtyInUnsavedWorkout(req, res) {
+//     const unsavedWorkout = await Workout.getUnsavedWorkout(req.user._id);
+//     await unsavedWorkout.setRepQty(req.body.exerciseId, req.body.newRepQty);
+//     res.json(unsavedWorkout)
+// }
 
 
 
-async function deleteSavedWorkout(req, res) {
-    await Workout.findOneAndDelete({ _id: req.params.id})
-    const workouts = await Workout.find({ user: req.user._id }).exec()
-    res.json(workouts)
-}
+// async function deleteSavedWorkout(req, res) {
+//     await Workout.findOneAndDelete({ _id: req.params.id})
+//     const workouts = await Workout.find({ user: req.user._id }).exec()
+//     res.json(workouts)
+// }
 
-async function editWorkout(req, res) {
-    const workoutToEdit = await Workout.findOne({ _id: req.body.workoutId })
-    workoutToEdit.isSaved = false;
-    console.log(workoutToEdit)
-    await workoutToEdit.save();
-    res.json(workoutToEdit)
-}
+// async function editWorkout(req, res) {
+//     const workoutToEdit = await Workout.findOne({ _id: req.body.workoutId })
+//     workoutToEdit.isSaved = false;
+//     console.log(workoutToEdit)
+//     await workoutToEdit.save();
+//     res.json(workoutToEdit)
+// }
--- models/workout.js
+++ models/workout.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { exerciseSchema } = require('./exercise')
 
-const exerciseChoiceSchema = new Schema({
-    // sets and reps will be strings to allow users to input range (ex. '8-10')
-    setQty: { type: String, default: '' },
-    repQty: { type: String, default: '' },
-    exercise: exerciseSchema
-}, {
-    timestamps: true
-})
+// const exerciseChoiceSchema = new Schema({
+//     // sets and reps will be strings to allow users to input range (ex. '8-10')
+//     setQty: { type: String, default: '' },
+//     repQty: { type: String, default: '' },
+//     exercise: exerciseSchema
+// }, {
+//     timestamps: true
+// })
 
 const workoutSchema =  new Schema({
     name: { type: String, default: '' },
@@ -18,46 +18,46 @@ const workoutSchema =  new Schema({
     timestamps: true
 })
 
-workoutSchema.statics.getUnsavedWorkout = function(userId) {
-    return this.findOneAndUpdate(
-        { user: userId, isSaved: false },
-        { user: userId },
-        { upsert: true, new: true }
-    )
-}
+// workoutSchema.statics.getUnsavedWorkout = function(userId) {
+//     return this.findOneAndUpdate(
+//         { user: userId, isSaved: false },
+//         { user: userId },
+//         { upsert: true, new: true }
+//     )
+// }
 
-workoutSchema.methods.addExerciseToUnsavedWorkout = async function(exerciseId) {
-    const unsavedWorkout = this;
-    const exerciseChoice = unsavedWorkout.exerciseChoices.find(exerciseChoice => exerciseChoice.exercise._id.equals(exerciseId));
-    if (exerciseChoice) {
-        return;
-    } else {
-        const Exercise = mongoose.model('Exercise');
-        const exercise = await Exercise.findById(exerciseId);
-        unsavedWorkout.exerciseChoices.push({exercise});
-    }
-    return unsavedWorkout.save();
-}
+// workoutSchema.methods.addExerciseToUnsavedWorkout = async function(exerciseId) {
+//     const unsavedWorkout = this;
+//     const exerciseChoice = unsavedWorkout.exerciseChoices.find(exerciseChoice => exerciseChoice.exercise._id.equals(exerciseId));
+//     if (exerciseChoice) {
+//         return;
+//     } else {
+//         const Exercise = mongoose.model('Exercise');
+//         const exercise = await Exercise.findById(exerciseId);
+//         unsavedWorkout.exerciseChoices.push({exercise});
+//     }
+//     return unsavedWorkout.save();
+// }
 
-workoutSchema.methods.setWorkoutName= function(workoutId, newWorkoutName) {
-    const unsavedWorkout = this;
-    this.name = newWorkoutName
-    return unsavedWorkout.save()
-}
+// workoutSchema.methods.setWorkoutName= function(workoutId, newWorkoutName) {
+//     const unsavedWorkout = this;
+//     this.name = newWorkoutName
+//     return unsavedWorkout.save()
+// }
 
-workoutSchema.methods.setSetQty = function(exerciseId, newSetQty) {
-    const unsavedWorkout = this;
-    const exerciseChoice = unsavedWorkout.exerciseChoices.find(exerciseChoice => exerciseChoice.exercise._id.equals(exerciseId))
-    exerciseChoice.setQty = newSetQty
-    return unsavedWorkout.save()
-}
+// workoutSchema.methods.setSetQty = function(exerciseId, newSetQty) {
+//     const unsavedWorkout = this;
+//     const exerciseChoice = unsavedWorkout.exerciseChoices.find(exerciseChoice => exerciseChoice.exercise._id.equals(exerciseId))
+//     exerciseChoice.setQty = newSetQty
+//     return unsavedWorkout.save()
+// }
 
-workoutSchema.methods.setRepQty = function(exerciseId, newRepQty) {
-    const unsavedWorkout = this;
-    const exerciseChoice = unsavedWorkout.exerciseChoices.find(exerciseChoice => exerciseChoice.exercise._id.equals(exerciseId))
-    exerciseChoice.repQty = newRepQty
-    return unsavedWorkout.save()
-}
+// workoutSchema.methods.setRepQty = function(exerciseId, newRepQty) {
+//     const unsavedWorkout = this;
+//     const exerciseChoice = unsavedWorkout.exerciseChoices.find(exerciseChoice => exerciseChoice.exercise._id.equals(exerciseId))
+//     exerciseChoice.repQty = newRepQty
+//     return unsavedWorkout.save()
+// }
 
 
 module.exports.workoutSchema = workoutSchema
