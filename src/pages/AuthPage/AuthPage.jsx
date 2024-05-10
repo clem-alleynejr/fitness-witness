78002c1d809798aef584c022be8681bba227b1af
clem-alleynejr <clem.alleynejr@gmail.com>
2024-05-10 13:22:39 -0600

    basic auth page css complete

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
-import SignUpForm from '../../components/SignUpForm/SignUpForm';
-import LoginForm from '../../components/LoginForm/LoginForm';
-import './AuthPage.css'
+import { useState } from "react";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import "./AuthPage.css";
 
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
 
   return (
-    <main className='auth-page'>
-
-      <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Existing User? Log In' : 'No Account? Sign Up'}</button>
-      {showSignUp ? 
+    <main className="auth-page">
+      {showSignUp ? (
         <SignUpForm setUser={setUser} />
-        :
+      ) : (
         <LoginForm setUser={setUser} />
-      }
+      )}
+
+      <div className="switch-form">
+        {showSignUp ? <p>Existing User?</p> : <p>No Account?</p>}
+
+        <button onClick={() => setShowSignUp(!showSignUp)}>
+          {showSignUp ? "Log In" : "Sign Up"}
+        </button>
+      </div>
     </main>
   );
 }
