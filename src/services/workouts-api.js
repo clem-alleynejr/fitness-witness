c2fccf4233c3288892e1c0be6d722c0e6b379143
clem-alleynejr <clem.alleynejr@gmail.com>
2024-05-14 17:06:59 -0600

    fixed mistake i made to fetch url for all workouts

--- src/services/workouts-api.js
+++ src/services/workouts-api.js
@@ -1,6 +1,6 @@
 import sendRequest from './send-request';
 
-const BASE_URL = 'https://exercisedb.p.rapidapi.com';
+const BASE_URL = '/api/workouts';
 
  export function getAll() {
    return sendRequest(BASE_URL);
