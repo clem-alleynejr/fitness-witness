4737ceb7f1d87b44cb5d1a89e2249b89845ce295
clem-alleynejr <clem.alleynejr@gmail.com>
2024-05-18 19:14:03 -0600

    INFINITE SCROLL OF EXERCISE OPTIONS  NOW WORKING :)

--- src/components/ExerciseList/ExerciseList.jsx
+++ src/components/ExerciseList/ExerciseList.jsx
@@ -1,18 +1,31 @@
-export default function ExerciseList({exercises, setQty, repQty }) {
+export default function ExerciseList({exercises, lastExerciseElementRef, setQty, repQty }) {
     return (
         <div className="exercise-list">
-        {exercises.map((exercise, index) => (
-          <div key={index} className="exercise">
-            <h6>{exercise.name}</h6>
-            <p>Target: {exercise.bodyPart}</p>
-            <p>Equipment: {exercise.equipment}</p>
-            {setQty && <p>Sets: {exercise.setQty}</p>}
-            {repQty && <p>Reps: {exercise.repQty}</p>}
-            {/* For Exercise Selector */}
-            <button>See More Details</button>
-            <button>Add to Workout</button>
-          </div>
-        ))}
+        {exercises.map((exercise, index) => {
+            if (index === exercises.length - 1) {
+                return <div key={index} ref={lastExerciseElementRef} className="exercise">
+                  <h6>{exercise.name}</h6>
+                  <p>Target: {exercise.bodyPart}</p>
+                  <p>Equipment: {exercise.equipment}</p>
+                  {setQty && <p>Sets: {exercise.setQty}</p>}
+                  {repQty && <p>Reps: {exercise.repQty}</p>}
+                  {/* For Exercise Selector */}
+                  <button>See More Details</button>
+                  <button>Add to Workout</button>
+                </div>
+            } else {
+                return <div key={index} className="exercise">
+                  <h6>{exercise.name}</h6>
+                  <p>Target: {exercise.bodyPart}</p>
+                  <p>Equipment: {exercise.equipment}</p>
+                  {setQty && <p>Sets: {exercise.setQty}</p>}
+                  {repQty && <p>Reps: {exercise.repQty}</p>}
+                  {/* For Exercise Selector */}
+                  <button>See More Details</button>
+                  <button>Add to Workout</button>
+                </div>
+            }
+        })}
       </div>
     );
 }
